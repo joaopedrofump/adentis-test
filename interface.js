64381dcdc07abd6df07510b88df3e05ebbf6a36d
Joao Furriel <joaofurriel@Joaos-MacBook-Pro.local>
2020-10-31 14:04:28 +0000

    web support

--- interface.js
+++ interface.js
@@ -1,5 +1,13 @@
-// import {palindrome} from './browser'
+const check = (inputs, testFunction, resultParagraph, concept) => resultParagraph.innerHTML = `The input phrase${inputs.length > 1 ? 's are' : ' is '} ${testFunction(...inputs.map(input => input.value)) ? ' ' : ' not '} ${concept}.`  
 
-const check = (arguments, testFunction) => testFunction(...arguments)
+const anagramArgs = Array.from(document.querySelectorAll('section#anagram input'))
+const palindromeArgs = Array.from(document.querySelectorAll('section#palindrome input'))
 
-const anagramArgs = document.querySelector('section#anagram input')
+const anagramButton = document.querySelector('section#anagram button')
+const palindromeButton = document.querySelector('section#palindrome button')
+
+const anagramAnswer = document.querySelector('section#anagram p')
+const palindromeAnswer = document.querySelector('section#palindrome p')
+
+anagramButton.addEventListener('click', () => check(anagramArgs,anagram,anagramAnswer,'anagrams'))
+palindromeButton.addEventListener('click', () => check(palindromeArgs,palindrome,palindromeAnswer, 'a palindrome'))
